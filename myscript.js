1f871051e7a6b8c369e818721b06d48fb48ee1ed
Linh Nguyen <nblinh86@gmail.com>
2026-07-18 22:18:40 +0000

    style div reposive

--- myscript.js
+++ myscript.js
@@ -6,3 +6,17 @@ function f()
 
 // Đang test thử cái này, test lại lần nữa.
 
+class MyHeader extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+    <header class="header">
+        <h1>Khám phá code</h1>
+        <div class="navbar">
+            <a href="index.html">Trang chủ</a> <!--Changed link here-->
+            <a href="#about">Giới thiệu</a>
+            <a href="#contact">Liên hệ</a>
+        </div>
+    </header>`;
+  }
+}
+customElements.define('my-header', MyHeader);
